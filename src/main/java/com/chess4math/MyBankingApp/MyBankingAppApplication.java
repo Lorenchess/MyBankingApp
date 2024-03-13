ddbc1cd825e3289b9d8d0de8e62fdb994bd7714f
Ramon Lorente <ramon_lorente@yahoo.com>
2024-03-13 07:01:23 -0400

    Setting up project dependencies.

---
+++ src/main/java/com/chess4math/MyBankingApp/MyBankingAppApplication.java
@@ -0,0 +1,13 @@
+package com.chess4math.MyBankingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBankingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBankingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chess4math/MyBankingApp/MyBankingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chess4math.MyBankingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBankingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
